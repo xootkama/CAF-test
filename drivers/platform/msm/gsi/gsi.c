3d49832be84b468534aa295cdda58f8d9f43dd1f
TheBeast <xootkama@gmail.com>
2022-01-07 16:23:45 +0000

    Revert "msm: gsi: Don't export various symbols"
    This reverts commit 35d24627261619a3824a05ad647443dd4f01532a.

--- drivers/platform/msm/gsi/gsi.c
+++ drivers/platform/msm/gsi/gsi.c
@@ -1969,6 +1969,7 @@ int gsi_write_channel_scratch(unsigned long chan_hdl,
 
 	return GSI_STATUS_SUCCESS;
 }
+EXPORT_SYMBOL(gsi_write_channel_scratch);
 
 int gsi_read_channel_scratch(unsigned long chan_hdl,
 	union __packed gsi_channel_scratch * ch_scratch)
@@ -2036,6 +2037,7 @@ int gsi_query_channel_db_addr(unsigned long chan_hdl,
 
 	return GSI_STATUS_SUCCESS;
 }
+EXPORT_SYMBOL(gsi_read_channel_scratch);
 
 int gsi_read_wdi3_channel_scratch2_reg(unsigned long chan_hdl,
 		union __packed gsi_wdi3_channel_scratch2_reg * val)
@@ -2101,6 +2103,7 @@ int gsi_update_mhi_channel_scratch(unsigned long chan_hdl,
 
 	return GSI_STATUS_SUCCESS;
 }
+EXPORT_SYMBOL(gsi_update_mhi_channel_scratch);
 
 int gsi_query_channel_db_addr(unsigned long chan_hdl,
 		uint32_t *db_addr_wp_lsb, uint32_t *db_addr_wp_msb)
@@ -2134,6 +2137,7 @@ int gsi_query_channel_db_addr(unsigned long chan_hdl,
 
 	return GSI_STATUS_SUCCESS;
 }
+EXPORT_SYMBOL(gsi_query_channel_db_addr);
 
 int gsi_start_channel(unsigned long chan_hdl)
 {
@@ -2186,6 +2190,7 @@ int gsi_start_channel(unsigned long chan_hdl)
 
 	return GSI_STATUS_SUCCESS;
 }
+EXPORT_SYMBOL(gsi_start_channel);
 
 int gsi_stop_channel(unsigned long chan_hdl)
 {
@@ -2269,6 +2274,7 @@ free_lock:
 	mutex_unlock(&gsi_ctx->mlock);
 	return res;
 }
+EXPORT_SYMBOL(gsi_stop_channel);
 
 int gsi_stop_db_channel(unsigned long chan_hdl)
 {
@@ -2337,6 +2343,7 @@ free_lock:
 	mutex_unlock(&gsi_ctx->mlock);
 	return res;
 }
+EXPORT_SYMBOL(gsi_stop_db_channel);
 
 int gsi_reset_channel(unsigned long chan_hdl)
 {
@@ -2420,6 +2427,7 @@ revrfy_chnlstate:
 
 	return GSI_STATUS_SUCCESS;
 }
+EXPORT_SYMBOL(gsi_reset_channel);
 
 int gsi_dealloc_channel(unsigned long chan_hdl)
 {
@@ -2482,6 +2490,7 @@ int gsi_dealloc_channel(unsigned long chan_hdl)
 
 	return GSI_STATUS_SUCCESS;
 }
+EXPORT_SYMBOL(gsi_dealloc_channel);
 
 void gsi_update_ch_dp_stats(struct gsi_chan_ctx *ctx, uint16_t used)
 {
@@ -2603,6 +2612,7 @@ int gsi_query_channel_info(unsigned long chan_hdl,
 
 	return GSI_STATUS_SUCCESS;
 }
+EXPORT_SYMBOL(gsi_query_channel_info);
 
 int gsi_is_channel_empty(unsigned long chan_hdl, bool *is_empty)
 {
@@ -2661,6 +2671,7 @@ int gsi_is_channel_empty(unsigned long chan_hdl, bool *is_empty)
 
 	return GSI_STATUS_SUCCESS;
 }
+EXPORT_SYMBOL(gsi_is_channel_empty);
 
 int gsi_queue_xfer(unsigned long chan_hdl, uint16_t num_xfers,
 		struct gsi_xfer_elem *xfer, bool ring_db)
@@ -2758,6 +2769,7 @@ int gsi_queue_xfer(unsigned long chan_hdl, uint16_t num_xfers,
 
 	return GSI_STATUS_SUCCESS;
 }
+EXPORT_SYMBOL(gsi_queue_xfer);
 
 int gsi_start_xfer(unsigned long chan_hdl)
 {
