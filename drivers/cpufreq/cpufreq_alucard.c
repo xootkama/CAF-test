cb8c977ac69315fad5ecfd9bc0abbf88be976904
TheBeast <xootkama@gmail.com>
2022-01-15 01:08:05 +0000

    Revert "cpufreq: Introduce cpufreq_start_governor()"
    This reverts commit 50250b72b7659009c692a801032f65d9226f4f4a.

--- drivers/cpufreq/cpufreq_alucard.c
+++ drivers/cpufreq/cpufreq_alucard.c
@@ -29,6 +29,7 @@
 #include <linux/ktime.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
+#include 
 /*
  * dbs is used in this file as a shortform for demandbased switching
  * It helps to keep variable names smaller, simpler
