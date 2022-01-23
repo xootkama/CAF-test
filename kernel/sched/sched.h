d5ad313a949fcee54266544378027cb3b02a239f
TheBeast <88646493+xootkama@users.noreply.github.com>
2022-01-23 16:28:22 +0530

    Update sched.h

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2850,11 +2850,6 @@ static inline enum sched_boost_policy task_boost_policy(struct task_struct *p)
 }
 static inline void check_for_migration(struct rq *rq, struct task_struct *p) { }
 
-static inline int sched_boost(void)
-{
-	return 0;
-}
-
 static inline bool hmp_capable(void) { return false; }
 static inline bool is_max_capacity_cpu(int cpu) { return true; }
 static inline bool is_min_capacity_cpu(int cpu) { return true; }
